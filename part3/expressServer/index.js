428611242c9849e8eb16d24dcfd2758dbca8acb5
[Tevfik] <tevfikhoglu@gmail.com>
2022-04-11 23:17:03 +0300

    part3

---
+++ part3/expressServer/index.js
@@ -0,0 +1,23 @@
+console.log('Hello World')
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2022-05-30T17:30:31.098Z',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2022-05-30T18:39:34.091Z',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2022-05-30T19:20:14.298Z',
+    important: true,
+  },
+]
+
